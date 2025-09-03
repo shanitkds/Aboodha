50ffa54737d7091074d6a766dec57499dfe2af57
Mohammed Shanith <shanith@example.com>
2025-09-02 23:41:57 -0700

    link- to- a -tag

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaFacebookF, FaInstagram, FaTimes, FaGithub, FaYoutube } from 'react-icons/fa';
 
 function Footer() {
   return (
-    <footer className="bg-[#e2e2f0] text-gray-800 py-6 w-screen ">
+    <footer className="bg-[#e2e2f0] text-gray-800 py-6 w-screen">
       <div className="container mx-auto flex flex-col items-center">
         {/* Social icons */}
         <div className="flex gap-6 mb-4">
-          <a href="#"><FaFacebookF className="hover:text-blue-600 transition-colors duration-300" /></a>
-          <a href="#"><FaInstagram className="hover:text-pink-500 transition-colors duration-300" /></a>
-          <a href="#"><FaTimes className="hover:text-black transition-colors duration-300" /></a>
-          <a href="#"><FaGithub className="hover:text-gray-900 transition-colors duration-300" /></a>
-          <a href="#"><FaYoutube className="hover:text-red-600 transition-colors duration-300" /></a>
+          <Link to="#">
+            <FaFacebookF className="hover:text-blue-600 transition-colors duration-300" />
+          </Link>
+          <Link to="#">
+            <FaInstagram className="hover:text-pink-500 transition-colors duration-300" />
+          </Link>
+          <Link to="#">
+            <FaTimes className="hover:text-black transition-colors duration-300" />
+          </Link>
+          <Link to="#">
+            <FaGithub className="hover:text-gray-900 transition-colors duration-300" />
+          </Link>
+          <Link to="#">
+            <FaYoutube className="hover:text-red-600 transition-colors duration-300" />
+          </Link>
         </div>
 
         {/* Footer text */}
